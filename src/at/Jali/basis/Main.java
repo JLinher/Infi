470a2e23874eb20efb4f04b663110cbd5fea4679
unknown <jan.linher@student.htldornbirn.at>
2023-10-11 10:51:43 +0200

    first commit

---
+++ src/at/Jali/basis/Main.java
@@ -0,0 +1,28 @@
+package at.Jali.basis;
+
+import java.awt.desktop.SystemSleepEvent;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo");
+
+        // short - ganze Zahl
+        // int - ganze Zahl
+        // float - Kommazahl
+        // double - Kommazahl (groß)
+        // boolean - true/false
+        // char - zecihen
+        // String - Zeichenkette
+
+        int number = 7;
+      String name = "Andi";
+
+        System.out.println(name);
+        System.out.println(number);
+
+
+    }
+
+    //strg blank
+}

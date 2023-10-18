92c816b9c8dbec2973920842c79dc7240e997008
Jan <jan.linher@student.htldornbirn.at>
2023-10-18 10:52:31 +0200

    Readme File erstellt

--- src/at/Jali/basis/ControlStructurs3.java
+++ src/at/Jali/basis/ControlStructurs3.java
@@ -9,12 +9,20 @@ public class ControlStructurs3 {
         int randomNumber = random.nextInt(100);
 
         // Gib die Zufallszahl aus
+        System.out.println("Die generierte Zufallszahl ist: " + randomNumber);
 
-        // Wenn die Zahl kleiner ist als 20  gib aus "Mini"
-        // Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
-        // Wenn die Zahl größer als 50 ist gib aus "Large"
-
-
+        // Wenn die Zahl kleiner ist als 20, gib "Mini" aus
+        if (randomNumber < 20) {
+            System.out.println("Mini");
+        }
+        // Wenn die Zahl zwischen 20 und 50 liegt, gib "Medium" aus
+        else if (randomNumber >= 20 && randomNumber <= 50) {
+            System.out.println("Medium");
+        }
+        // Wenn die Zahl größer als 50 ist, gib "Large" aus
+        else {
+            System.out.println("Large");
+        }
     }
 }
 
